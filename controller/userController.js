1acbe0fc0413fe615a096b68c6b37409551f206b
AnanSer <ananserbesa@gmail.com>
2025-01-07 10:47:52 +0300

    Add .npmignore files, README updates, and new module implementations; update package.json dependencies

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,54 @@
 const dbConnection = require("../db/dbConfig");
 
+const bcyrypt = require("bcrypt");
+
+const { StatusCodes } = require("http-status-codes");
+
 async function register(req, res) {
   const { username, firstname, lastname, email, password } = req.body;
 
   if (!email || !password || !username || !firstname || !lastname) {
-    return res.status(401).json({ msg: "provide all required fields" });
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "provide all required fields" });
+  }
+
+  try {
+    const [user] = await dbConnection.query(
+      "SELECT * FROM users WHERE username = ? OR email = ?",
+      [username, email]
+    );
+
+    if (user.length > 0)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "user already exists" });
+
+    if (password.length < 6) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "password must be at least 6 characters" });
+
+      return res.json({ users: user });
+    }
+
+    // encrypt password
+    const salt = await bcyrypt.genSalt(10);
+    const hashedPassword = await bcyrypt.hash(password, salt);
+
+    await dbConnection.query(
+      "INSERT INTO users (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)",
+      [username, firstname, lastname, email, hashedPassword]
+    );
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "user registered successfully" });
+  } catch (err) {
+    console.error("Database error:", err.message);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      msg: "something went wrong, try again later",
+      error: err.message,
+    });
   }
 }
 

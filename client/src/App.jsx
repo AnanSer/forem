a34f5bcce1dce78e17d27bc0fd3d645cbb09744b
AnanSer <ananserbesa@gmail.com>
2025-01-11 22:13:49 +0300

    Add user authentication check and context provider to manage user state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,59 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { useEffect, useState, createContext } from "react";
+import axios from "./axiosConfig";
+
+export const AppState = createContext();
 
 const App = () => {
+  const [user, setUser] = useState({});
+
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  async function checkuser() {
+    if (!token) {
+      console.log("No token found, redirecting to login");
+      navigate("/login");
+      return;
+    }
+
+    try {
+      const { data } = await axios.get("/users/checkuser", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+
+      setUser(data);
+    } catch (error) {
+      if (error.response) {
+        console.log("Error response:", error.response);
+      } else if (error.request) {
+        console.log("Error request:", error.request);
+      } else {
+        console.log("Error message:", error.message);
+      }
+      navigate("/login");
+    }
+  }
+
+  // check if user is logged in or no one cannot access the home page
+  useEffect(() => {
+    checkuser();
+  }, []);
+
   return (
-    <div>
+    <AppState.Provider value={{ user, setUser }}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </div>
+    </AppState.Provider>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+import { AppState } from "../App";
+
 function Home() {
+  useContext(AppState);
   return (
     <div>
       <h1>Home</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h2>{"username"}</h2>
     </div>
   );
 }
